64fb6be3caae98ee14d831710874b880deb410dc
플랫폼, LMS 구축 전문, Github 기반 Project <kimdypm@gmail.com>
2024-04-14 16:00:01 +0900

    1

--- example/src/main/java/odin/example/applicationservices/rest/PersonController.java
+++ example/src/main/java/odin/example/applicationservices/rest/PersonController.java
@@ -4,6 +4,7 @@ package odin.example.applicationservices.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import odin.example.readmodel.PersistableReadModelPerson;
@@ -19,5 +20,11 @@ public class PersonController {
     public PersistableReadModelPerson getBook(@PathVariable int id) {
         return personService.findPersonById(id);
     }
+
+    @GetMapping("/person1")    
+    public PersistableReadModelPerson person1(@RequestParam(value="id") int id) {        
+        return personService.findPersonById(id);
+    }
+
 
 }
